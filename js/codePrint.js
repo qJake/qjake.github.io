9131ecc5da2d4a779c5255c5c45abd500a473f81
qJake <jake@burgy.me>
2016-01-07 21:19:47 -0600

    First major release of the site

--- js/codePrint.js
+++ js/codePrint.js
@@ -37,39 +37,16 @@ using System.Windows.Input;\r\n\
 namespace Molten.Core.Wpf.DragDrop\r\n\
 {\r\n\
     /// <summary>\r\n\
-    /// Encapsulates a single drag/drop operation between any number of drop sources and any number of drop targets.\r\n\
+    /// Encapsulates a single drag/drop operation between any number of drop sources\r\n\
+    /// and any number of drop targets.\r\n\
     /// </summary>\r\n\
     public class DragDropper\r\n\
     {\r\n\
         /// <summary>\r\n\
-        /// Specifies the unique name of this drag/drop operation. This is used during the drag/drop operation to detect the contents of the drop.\r\n\
+        /// Specifies the unique name of this drag/drop operation. This is used during\r\n\
+        /// the drag/drop operation to detect the contents of the drop.\r\n\
         /// </summary>\r\n\
         public string Name { get; set; }\r\n\
-\r\n\
-        /// <summary>\r\n\
-        /// Occurs when the drop event is fired on a valid target. The parameters are: package, source, target.\r\n\
-        /// </summary>\r\n\
-        public event Action<DropEventArgs> Drop;\r\n\
-\r\n\
-        /// <summary>\r\n\
-        /// Occurs when the drag event starts. The UIElement parameter is the source (to match it with the source in the DragDropper) and the object is the package to include.\r\n\
-        /// </summary>\r\n\
-        public event Action<DragStartEventArgs> DragStart;\r\n\
-\r\n\
-        /// <summary>\r\n\
-        /// Occurs when trying to drop a source onto a target. The \"AllowDrop\" property in the DropEventArgs dictates if the drop will occur or be cancelled.\r\n\
-        /// </summary>\r\n\
-        public event Action<DropEventArgs> CanDrop;\r\n\
-\r\n\
-        /// <summary>\r\n\
-        /// Occurs if the result of CanDrop is true, when a source is dragged over this target.\r\n\
-        /// </summary>\r\n\
-        public event Action DragEnter;\r\n\
-\r\n\
-        /// <summary>\r\n\
-        /// Occurs if the result of CanDrop is true, when a source is dragged away from this target.\r\n\
-        /// </summary>\r\n\
-        public event Action DragLeave;\r\n\
 \r\n\
         private List<UIElement> Sources { get; set; }\r\n\
         private List<UIElement> Targets { get; set; }\r\n\
@@ -83,8 +60,9 @@ namespace Molten.Core.Wpf.DragDrop\r\n\
         /// <summary>\r\n\
         /// Initializes a new instance of the DragDropper class.\r\n\
         /// \r\n\
-        /// This class may only be initialized from within this assembly. To use the DragDropper class, refer to the <see cref=\"DragDropManager\" />\r\n\
-        /// to create a new instance of this class.\r\n\
+        /// This class may only be initialized from within this assembly. To use the\r\n\
+        /// DragDropper class, refer to the <see cref=\"DragDropManager\" /> to create\r\n\
+        /// a new instance of this class.\r\n\
         /// </summary>\r\n\
         /// <param name=\"name\">The unique name of this DragDropper (to distinguish multiple drag/drop events).</param>\r\n\
         internal DragDropper(string name)\r\n\
@@ -117,215 +95,7 @@ namespace Molten.Core.Wpf.DragDrop\r\n\
             Sources.Add(source);\r\n\
         }\r\n\
 \r\n\
-        /// <summary>\r\n\
-        /// Removes a valid source drag target.\r\n\
-        /// </summary>\r\n\
-        /// <param name=\"source\">The source to remove.</param>\r\n\
-        public void RemoveSource(UIElement source)\r\n\
-        {\r\n\
-            source.PreviewMouseLeftButtonDown -= Source_MouseDown;\r\n\
-            source.MouseMove -= Source_MouseMove;\r\n\
-            source.PreviewMouseLeftButtonUp -= Source_MouseUp;\r\n\
-            Sources.Remove(source);\r\n\
-        }\r\n\
-\r\n\
-        /// <summary>\r\n\
-        /// Adds a valid drop target.\r\n\
-        /// </summary>\r\n\
-        /// <param name=\"target\">The element which can accept any of the specified <see cref=\"Sources\" /> during a drop operation.</param>\r\n\
-        public void AddTarget(UIElement target)\r\n\
-        {\r\n\
-            target.DragEnter += Target_DragEnter;\r\n\
-            target.DragLeave += Target_DragLeave;\r\n\
-            target.DragOver += Target_DragOver;\r\n\
-            target.Drop += Target_Drop;\r\n\
-            target.AllowDrop = true;\r\n\
-            Targets.Add(target);\r\n\
-        }\r\n\
-\r\n\
-        /// <summary>\r\n\
-        /// Removes the specified drop target from the list of valid drop targets.\r\n\
-        /// </summary>\r\n\
-        /// <param name=\"target\">The specified element to remove from the list of drop targets.</param>\r\n\
-        public void RemoveTarget(UIElement target)\r\n\
-        {\r\n\
-            target.DragEnter -= Target_DragEnter;\r\n\
-            target.DragLeave -= Target_DragLeave;\r\n\
-            target.DragOver -= Target_DragOver;\r\n\
-            target.Drop -= Target_Drop;\r\n\
-            target.AllowDrop = false;\r\n\
-            Targets.Remove(target);\r\n\
-        }\r\n\
-\r\n\
-        #region Source Events\r\n\
-\r\n\
-        private void Source_MouseDown(object s, MouseEventArgs e)\r\n\
-        {\r\n\
-            if (IsInitialized())\r\n\
-            {\r\n\
-                StartPoint = e.GetPosition(null);\r\n\
-                IsMouseDetecting = true;\r\n\
-            }\r\n\
-        }\r\n\
-\r\n\
-        private void Source_MouseMove(object s, MouseEventArgs e)\r\n\
-        {\r\n\
-            if (IsInitialized())\r\n\
-            {\r\n\
-                if (!IsDragging)\r\n\
-                {\r\n\
-                    if (IsMouseDetecting)\r\n\
-                    {\r\n\
-                        // Get the current mouse position\r\n\
-                        Point mousePos = e.GetPosition(null);\r\n\
-                        Vector diff = StartPoint - mousePos;\r\n\
-\r\n\
-                        if (e.LeftButton == MouseButtonState.Pressed &&\r\n\
-                            Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||\r\n\
-                            Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)\r\n\
-                        {\r\n\
-                            DragStartEventArgs ea = new DragStartEventArgs();\r\n\
-\r\n\
-                            // Invoke the DragStart event on the source to allow it to set the package contents.\r\n\
-                            DragStart.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke(ea);\r\n\
-\r\n\
-                            DataObject dragData = new DataObject(Name, ea.Package);\r\n\
-\r\n\
-                            IsMouseDetecting = false;\r\n\
-                            IsDragging = true;\r\n\
-                            currentSource = s as UIElement;\r\n\
-\r\n\
-                            // This blocks until the drop operation completes.\r\n\
-                            System.Windows.DragDrop.DoDragDrop((s as UIElement), dragData, DragDropEffects.Move | DragDropEffects.None);\r\n\
-\r\n\
-                            IsDragging = false;\r\n\
-                            currentSource = null;\r\n\
-                        }\r\n\
-                    }\r\n\
-                }\r\n\
-            }\r\n\
-        }\r\n\
-\r\n\
-        private void Source_MouseUp(object s, MouseEventArgs e)\r\n\
-        {\r\n\
-            if (IsInitialized())\r\n\
-            {\r\n\
-                // Handle the case where the user depresses the mouse button, moves less than SystemParameters.Minimum*DragDistance and then releases the mouse button.\r\n\
-                IsMouseDetecting = false;\r\n\
-                IsDragging = false;\r\n\
-                currentSource = null;\r\n\
-            }\r\n\
-        }\r\n\
-\r\n\
-        #endregion\r\n\
-\r\n\
-        #region Target Events\r\n\
-\r\n\
-        private void Target_DragEnter(object s, DragEventArgs e)\r\n\
-        {\r\n\
-            if (IsInitialized())\r\n\
-            {\r\n\
-                if (IsDragging)\r\n\
-                {\r\n\
-                    e.Handled = true;\r\n\
-                    DropEventArgs ea = new DropEventArgs()\r\n\
-                    {\r\n\
-                        Package = e.Data.GetData(Name),\r\n\
-                        Source = currentSource\r\n\
-                    };\r\n\
-\r\n\
-                    CanDrop.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke(ea);\r\n\
-\r\n\
-                    if (ea.AllowDrop)\r\n\
-                    {\r\n\
-                        DragEnter.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke();\r\n\
-                    }\r\n\
-                }\r\n\
-            }\r\n\
-        }\r\n\
-\r\n\
-        private void Target_DragOver(object s, DragEventArgs e)\r\n\
-        {\r\n\
-            if (IsInitialized())\r\n\
-            {\r\n\
-                if (IsDragging)\r\n\
-                {\r\n\
-                    e.Handled = true;\r\n\
-\r\n\
-                    DropEventArgs ea = new DropEventArgs()\r\n\
-                    {\r\n\
-                        Package = e.Data.GetData(Name),\r\n\
-                        Source = currentSource\r\n\
-                    };\r\n\
-\r\n\
-                    CanDrop.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke(ea);\r\n\
-\r\n\
-                    if (ea.AllowDrop)\r\n\
-                    {\r\n\
-                        e.Effects = DragDropEffects.Move;\r\n\
-                    }\r\n\
-                    else\r\n\
-                    {\r\n\
-                        e.Effects = DragDropEffects.None;\r\n\
-                    }\r\n\
-                }\r\n\
-            }\r\n\
-        }\r\n\
-\r\n\
-        private void Target_DragLeave(object s, DragEventArgs e)\r\n\
-        {\r\n\
-            if (IsInitialized())\r\n\
-            {\r\n\
-                if (IsDragging)\r\n\
-                {\r\n\
-                    e.Handled = true;\r\n\
-                    DropEventArgs ea = new DropEventArgs()\r\n\
-                    {\r\n\
-                        Package = e.Data.GetData(Name),\r\n\
-                        Source = currentSource\r\n\
-                    };\r\n\
-\r\n\
-                    CanDrop.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke(ea);\r\n\
-\r\n\
-                    if (ea.AllowDrop)\r\n\
-                    {\r\n\
-                        DragLeave.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke();\r\n\
-                    }\r\n\
-                }\r\n\
-            }\r\n\
-        }\r\n\
-\r\n\
-        private void Target_Drop(object s, DragEventArgs e)\r\n\
-        {\r\n\
-            if (IsInitialized())\r\n\
-            {\r\n\
-                if (IsDragging)\r\n\
-                {\r\n\
-                    e.Handled = true;\r\n\
-                    if (e.Data.GetDataPresent(Name))\r\n\
-                    {\r\n\
-                        DropEventArgs ea = new DropEventArgs()\r\n\
-                        {\r\n\
-                            Package = e.Data.GetData(Name),\r\n\
-                            Source = currentSource\r\n\
-                        };\r\n\
-\r\n\
-                        // Invoke the CanDrop event again to ask the target if we're allowed to drop\r\n\
-                        CanDrop.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke(ea);\r\n\
-\r\n\
-                        if (ea.AllowDrop)\r\n\
-                        {\r\n\
-                            Drop.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke(ea);\r\n\
-                            // Also fire DragLeave since the system won't do this for us.\r\n\
-                            DragLeave.GetInvocationList().Where(d => d.Target == s).First().DynamicInvoke();\r\n\
-                        }\r\n\
-                    }\r\n\
-                }\r\n\
-            }\r\n\
-        }\r\n\
-\r\n\
-        #endregion\r\n\
-    }\r\n\
+        ..................\r\n\
 }";
     };
 })();
--- js/site.js
+++ js/site.js
@@ -1,6 +1,29 @@
 (function() {
     $(function()
     {
-       $('.full-height').height($(document).height()); 
+        // Set container height to browser height
+        // TODO: Re-apply when window size changes
+        $(window).resize(function()
+        {
+            $('.full-height').height($(window).height());
+        })
+        $(window).resize();
+        
+        var scrollMsg = setTimeout(function()
+        {
+            // After a few moments, show the "Scroll" message
+            $('#scrollmsg').addClass('active');
+        }, 6500);
+        
+        // If they scroll, don't bother showing the message. And hide it if it's already there.
+        $(document).scroll(function()
+        {
+           if (scrollMsg)
+           {
+               clearTimeout(scrollMsg);
+               scrollMsg = null;
+           }
+           $('#scrollmsg').removeClass('active'); 
+        });
     });
 })();
