582136fb01541ed4d549957bfa9aa619cc3a40d9
qJake <jake@burgy.me>
2016-01-08 10:53:43 -0600

    Version 1.1 with projects and icons

--- js/site.js
+++ js/site.js
@@ -1,4 +1,23 @@
 (function() {
+    var icons = [
+        {file: 'vs', name: 'Visual Studio'},
+        {file: 'html5', name: 'HTML5'},
+        {file: 'css3', name: 'CSS3'},
+        {file: 'angular', name: 'Angular'},
+        {file: 'less', name: 'LESS'},
+        {file: 'bootstrap', name: 'Bootstrap'},
+        {file: 'git', name: 'Git'},
+        {file: 'github', name: 'GitHub'},
+        {file: 'windows', name: 'Windows'},
+        {file: 'powershell', name: 'PowerShell'},
+        {file: 'sqlserver', name: 'SQL Server'},
+        {file: 'msft', name: 'Microsoft'},
+        {file: 'nodejs', name: 'Node.js'},
+        {file: 'office', name: 'Office'},
+        {file: 'onedrive', name: 'OneDrive / Azure'},
+        {file: 'adobecc', name: 'Adobe CC'},
+    ];
+        
     $(function()
     {
         // Set container height to browser height
@@ -25,5 +44,16 @@
            }
            $('#scrollmsg').removeClass('active'); 
         });
+        
+        // Insert the icons in the icon area
+        var iconArea = $('#icons div');
+        for(var i = 0; i < icons.length; i++)
+        {
+            iconArea.append(
+                $('<img />').attr('src', "images/icons/" + icons[i].file + ".png")
+                            .attr('alt', icons[i].name)
+                            .attr('title', icons[i].name)
+            );
+        }
     });
 })();
